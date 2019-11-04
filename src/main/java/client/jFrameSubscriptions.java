18882b842e23cb27261837f4de514831265b10bf
Esteban <darklightbeing@gmail.com>
2019-11-03 19:54:00 -0600

    Subscripciones de Cliente
    Se implementa la pantalla de subscripciones del cliente.

--- src/main/java/client/jFrameSubscriptions.java
+++ src/main/java/client/jFrameSubscriptions.java
@@ -5,6 +5,10 @@
  */
 package client;
 
+import javax.swing.DefaultCellEditor;
+import javax.swing.JComboBox;
+import javax.swing.table.TableColumn;
+
 /**
  *
  * @author Esteban
@@ -28,29 +32,53 @@ public class jFrameSubscriptions extends javax.swing.JFrame {
   private void initComponents() {
 
     jScrollPane1 = new javax.swing.JScrollPane();
-    jTable1 = new javax.swing.JTable();
+    jTableSubscripciones = new javax.swing.JTable();
     jButtonAddSubscription = new javax.swing.JButton();
     jButtonRemoveSubscription = new javax.swing.JButton();
+    jButtonCancel = new javax.swing.JButton();
+    jButtonCommitChanges = new javax.swing.JButton();
+    jButtonEndedSubscriptions = new javax.swing.JButton();
 
     setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
     setTitle("Your Subscriptions");
 
-    jTable1.setModel(new javax.swing.table.DefaultTableModel(
+    jTableSubscripciones.setModel(new javax.swing.table.DefaultTableModel(
       new Object [][] {
-        {null, null, null, null},
-        {null, null, null, null},
-        {null, null, null, null},
         {null, null, null, null}
       },
       new String [] {
-        "Title 1", "Title 2", "Title 3", "Title 4"
+        "Subscription Type", "Date Requested", "Date Accepted", "Price"
       }
-    ));
-    jScrollPane1.setViewportView(jTable1);
+    ) {
+      Class[] types = new Class [] {
+        java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class
+      };
+      boolean[] canEdit = new boolean [] {
+        true, false, true, false
+      };
+
+      public Class getColumnClass(int columnIndex) {
+        return types [columnIndex];
+      }
+
+      public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return canEdit [columnIndex];
+      }
+    });
+    jScrollPane1.setViewportView(jTableSubscripciones);
+    if (jTableSubscripciones.getColumnModel().getColumnCount() > 0) {
+      jTableSubscripciones.getColumnModel().getColumn(0).setCellEditor(this.getSubscriptionCellEditor());
+    }
 
     jButtonAddSubscription.setText("Add Subscription");
 
     jButtonRemoveSubscription.setText("Remove Subscription");
+
+    jButtonCancel.setText("Cancel");
+
+    jButtonCommitChanges.setText("Commit Changes");
+
+    jButtonEndedSubscriptions.setText("View Finalized Subscriptions");
 
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
@@ -62,18 +90,27 @@ public class jFrameSubscriptions extends javax.swing.JFrame {
         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
           .addComponent(jButtonRemoveSubscription, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-          .addComponent(jButtonAddSubscription, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+          .addComponent(jButtonAddSubscription, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+          .addComponent(jButtonCancel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+          .addComponent(jButtonCommitChanges, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+          .addComponent(jButtonEndedSubscriptions, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
     );
     layout.setVerticalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
       .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
         .addContainerGap(13, Short.MAX_VALUE)
-        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
           .addGroup(layout.createSequentialGroup()
             .addComponent(jButtonAddSubscription)
             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-            .addComponent(jButtonRemoveSubscription))
+            .addComponent(jButtonRemoveSubscription)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButtonEndedSubscriptions)
+            .addGap(18, 18, 18)
+            .addComponent(jButtonCancel)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+            .addComponent(jButtonCommitChanges))
           .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 341, javax.swing.GroupLayout.PREFERRED_SIZE))
         .addContainerGap())
     );
@@ -81,6 +118,33 @@ public class jFrameSubscriptions extends javax.swing.JFrame {
     pack();
   }// </editor-fold>//GEN-END:initComponents
 
+  // <editor-fold defaultstate="collapsed" desc="Event Handlers">
+  
+  // </editor-fold>
+  
+  // <editor-fold defaultstate="collapsed" desc="Private methods">
+  /**
+   * This method builds a new editor for the subscription column based on a 
+   * combobox
+   * @return A new cell editor based on the combobox editor
+   */
+  private DefaultCellEditor getSubscriptionCellEditor(){
+    //@TODO: Build subscriptions on actual database data
+
+    //First I need to build an array on the subscription types
+    Object [] subscriptions = {
+      "Daily",
+      "Weekly",
+      "Monthly",
+      "Nocturnal"
+    };
+    //Then I build a new ComboBox, which will be use to edit this column
+    JComboBox subscriptionComboBox = new JComboBox(subscriptions);
+
+    return new DefaultCellEditor(subscriptionComboBox);
+  }
+  // </editor-fold>
+  
   /**
    * @param args the command line arguments
    */
@@ -118,8 +182,11 @@ public class jFrameSubscriptions extends javax.swing.JFrame {
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JButton jButtonAddSubscription;
+  private javax.swing.JButton jButtonCancel;
+  private javax.swing.JButton jButtonCommitChanges;
+  private javax.swing.JButton jButtonEndedSubscriptions;
   private javax.swing.JButton jButtonRemoveSubscription;
   private javax.swing.JScrollPane jScrollPane1;
-  private javax.swing.JTable jTable1;
+  private javax.swing.JTable jTableSubscripciones;
   // End of variables declaration//GEN-END:variables
 }
